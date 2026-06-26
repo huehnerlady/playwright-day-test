739ef629c36e8132f4e102d1efef55524741e17e
Ruth Bassindale <ruth@BiRuRa.fritz.box>
2026-06-26 18:32:37 +0200

    first commit

---
+++ tests/example.spec.ts
@@ -0,0 +1,30 @@
+import { test, expect } from '@playwright/test';
+
+// test('has title', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
+//
+//   // Expect a title "to contain" a substring.
+//   await expect(page).toHaveTitle(/Playwright/);
+// });
+//
+// test('get started link', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
+//
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'Get started' }).click();
+//
+//   // Expects page to have a heading with the name of Installation.
+//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+// });
+
+test('tag1', async ({ page }) => {
+  await page.goto('http://www.cuba-hp.de/24Stunden/index.php?code=2593b7089f7b083ead87f4cec823fd43');
+
+  await page.screenshot({ path: 'screenshot-tag1.png', fullPage: true });
+});
+
+test('tag2', async ({ page }) => {
+  await page.goto('http://www.cuba-hp.de/24Stunden2/index.php?code=2fd139f9c633e9847aee9e10d1a10340');
+
+  await page.screenshot({ path: 'screenshot-tag2.png', fullPage: true });
+});
