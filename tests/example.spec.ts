ce3a892f1e09780154bf0090cbbf54880a878bd5
Ruth <github@huehnerlady.de>
2026-06-27 06:50:30 +0200

    Add solution writing into script

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,42 +1,43 @@
-import { test, expect } from '@playwright/test';
+import {test, expect} from '@playwright/test';
 import * as fs from "node:fs";
 import {join} from "node:path";
 
-// test('has title', async ({ page }) => {
-//   await page.goto('https://playwright.dev/');
-//
-//   // Expect a title "to contain" a substring.
-//   await expect(page).toHaveTitle(/Playwright/);
-// });
-//
-// test('get started link', async ({ page }) => {
-//   await page.goto('https://playwright.dev/');
-//
-//   // Click the get started link.
-//   await page.getByRole('link', { name: 'Get started' }).click();
-//
-//   // Expects page to have a heading with the name of Installation.
-//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-// });
+const testData = [
+    {
+        description: 'tag1',
+        url: 'http://www.cuba-hp.de/24Stunden/index.php?code=2593b7089f7b083ead87f4cec823fd43',
+        imageHost: 'http://www.cuba-hp.de/24Stunden',
+        solution: ''
+    },
+    {
+        description: 'tag2',
+        url: 'http://www.cuba-hp.de/24Stunden2/index.php?code=2fd139f9c633e9847aee9e10d1a10340',
+        imageHost: 'http://www.cuba-hp.de/24Stunden2',
+        solution: ''
+    },
+    // {
+    //     description: 'tag3',
+    //     url: 'https://www.cuba-hp.de/24Stunden3/index.php?code=bfb93b6dfcb468756e479beb490040ff2616',
+    //     imageHost: 'http://www.cuba-hp.de/24Stunden3',
+    //     solution: 'Rom Kairo Paris Wien Madrid San Francisco Sydney Montreal'
+    // },
+];
 
-test('tag1', async ({ page }) => {
-  await page.goto('http://www.cuba-hp.de/24Stunden/index.php?code=2593b7089f7b083ead87f4cec823fd43');
+for (const {description, url, solution, imageHost} of testData) {
+    test(description, async ({page}) => {
+        await page.goto(url);
 
-  await page.screenshot({ path: 'screenshot-tag1.png', fullPage: true });
-  const html = await page.content()
-  // html.replace('./images', 'https://www.cuba-hp.de/24Stunden2/images')
-  fs.writeFileSync(join(__dirname, 'tag1.html'), html, {
-    flag: 'w',
-  });
-});
+        if (solution !== '') {
+            await expect(page.locator('input[type=text]')).toBeVisible();
+            await page.locator('input[type=text]').fill(solution);
+            await page.locator('input[type=submit]').click();
+        }
+        await page.screenshot({path: `screenshot-${description}.png`, fullPage: true});
+        let html = await page.content();
+        html = html.replace(/\.\/images/g, `${imageHost}/images`);
+        fs.writeFileSync(join(__dirname, `${description}.html`), html, {
+            flag: 'w',
+        });
+    });
+}
 
-test('tag2', async ({ page }) => {
-  await page.goto('http://www.cuba-hp.de/24Stunden2/index.php?code=2fd139f9c633e9847aee9e10d1a10340');
-
-  await page.screenshot({ path: 'screenshot-tag2.png', fullPage: true });
-  const html = await page.content()
-  // html.replace('./images', 'https://www.cuba-hp.de/24Stunden2/images')
-  fs.writeFileSync(join(__dirname, 'tag2.html'), html, {
-    flag: 'w',
-  });
-});
