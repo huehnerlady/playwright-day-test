1266c3f2ad123568c9fc4f4b13402cae86341dbd
Ruth <github@huehnerlady.de>
2026-07-15 21:10:31 +0200

    Support multiple solutions, adapt solution for day2, add new solution for day1

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,46 +1,57 @@
-import {test, expect} from '@playwright/test';
+import {test, expect, Page} from '@playwright/test';
 import * as fs from "node:fs";
 import {join} from "node:path";
 
-const testData = [
+type TestData = {
+    description: string,
+    url: string,
+    imageHost: string,
+    solutions?: string[]
+}
+
+const testData: TestData[] = [
     {
         description: 'tag1',
         url: 'http://www.cuba-hp.de/24Stunden/index.php?code=d8bc48b4cadefa1c6960f229606faf61',
         imageHost: 'http://www.cuba-hp.de/24Stunden',
-        solution: ''
+        solutions: ['126389', '126399']
     },
     {
         description: 'tag2',
         url: 'http://www.cuba-hp.de/24Stunden2/index.php?code=d8456656c41b0c0b24a6527a0608d832',
         imageHost: 'http://www.cuba-hp.de/24Stunden2',
-        solution: '434'
-    },
-    // {
-    //     description: 'tag3',
-    //     url: 'https://www.cuba-hp.de/24Stunden3/index.php?code=bfb93b6dfcb468756e479beb490040ff2616',
-    //     imageHost: 'http://www.cuba-hp.de/24Stunden3',
-    //     solution: 'Rom Kairo Paris Wien Madrid San Francisco Sydney Montreal'
-    // },
+        solutions: ['412']
+    }
 ];
 
-for  (const {description, url, solution, imageHost} of testData) {
+const takeData = async (suffix: string, imageHost: string, page: Page): Promise<void> => {
+    await page.screenshot({path: `screenshot-${suffix}.png`, fullPage: true});
+    let html = await page.content();
+    html = html.replace(/\.\/images/g, `${imageHost}/images`);
+    fs.writeFileSync(join(__dirname, `${suffix}.html`), html, {
+        flag: 'w',
+    });
+}
+
+
+for (const {description, url, solutions, imageHost} of testData) {
     test(description, async ({page}) => {
         await page.goto(url);
 
         const input = page.locator('input[type=text]');
 
-        if (solution !== '' && await input.count() > 0) {
-            await expect(input).toBeVisible();
-            await input.fill(solution);
-            await page.locator('input[type=submit]').click();
+        if (solutions.length > 0 && await input.count() > 0) {
+            for (const solution of solutions) {
+                const index = solutions.indexOf(solution);
+                await expect(input).toBeVisible();
+                await input.fill(solution);
+                await page.locator('input[type=submit]').click();
+
+                await takeData(`${description}-${index}`, imageHost, page);
+            }
+        } else {
+            await takeData(description, imageHost, page);
         }
-        
-        await page.screenshot({path: `screenshot-${description}.png`, fullPage: true});
-        let html = await page.content();
-        html = html.replace(/\.\/images/g, `${imageHost}/images`);
-        fs.writeFileSync(join(__dirname, `${description}.html`), html, {
-            flag: 'w',
-        });
     });
 }
 
