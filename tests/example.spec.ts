b71878aa72aa9324810cb34f09ff886c2f1bc812
Ruth <github@huehnerlady.de>
2026-07-27 19:38:08 +0200

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -33,7 +33,7 @@ const testData: TestData[] = [
         description: 'tag2',
         url: 'http://www.cuba-hp.de/24Stunden2/index.php?code=f7bcfdbf4059374fe5054e0f8f591c90',
         imageHost: 'http://www.cuba-hp.de/24Stunden2',
-        solutions: ['SMARSLSHPTSBHBS']
+        solutions: ['SMARSLSHPTRBHBS']
     }
 ];
 
