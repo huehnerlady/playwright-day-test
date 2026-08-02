f5ca6a00f4e9eccb626655be3f99450c1d6de3a1
Ruth <github@huehnerlady.de>
2026-08-02 16:41:52 +0200

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -6,7 +6,7 @@ import {TestData} from "./utils";
 const testData: TestData[] = [
     {
         description: 'tag1',
-        url: 'https://www.cuba-hp.de/24Stunden/index.php?code=4b473350faefa6991ed520909d18f6c7',
+        url: 'http://www.cuba-hp.de/24Stunden/index.php?code=8f62af03d748cc4d00856f5a84986adb',
         imageHost: 'http://www.cuba-hp.de/24Stunden',
         solutions: ['']
     },
