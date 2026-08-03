18806e7d1611856b25f02a60e703a3c945dd28b8
Ruth <github@huehnerlady.de>
2026-08-03 07:42:48 +0200

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -12,9 +12,9 @@ const testData: TestData[] = [
     },
     {
         description: 'tag2',
-        url: 'https://www.cuba-hp.de/24Stunden2/index.php?code=188264781fbce1cd02074bcc6ce9052c',
+        url: 'http://www.cuba-hp.de/24Stunden2/index.php?code=7b0f417c6b09bb2607ea8c254b16af91',
         imageHost: 'http://www.cuba-hp.de/24Stunden2',
-        solutions: ['32876']
+        solutions: ['']
     }
 ];
 
